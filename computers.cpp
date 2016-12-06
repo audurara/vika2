4f55bfc613132e6ca92cda5d987724b1d11cd039
Olafur <bright>
2016-12-06 10:35:20 +0000

    (baetti vid computerklasa)

---
+++ computers.cpp
@@ -0,0 +1,16 @@
+#include "computers.h"
+
+computers::computers()
+{
+
+}
+
+
+computers::computers(Qstring name, Qstring type, Qstring bYear, Qstring constr)
+{
+   _name = name;
+   _type = type;
+   _bYear = bYear;
+   _constr = constr;
+
+}
